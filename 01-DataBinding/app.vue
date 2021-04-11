6ef8f5660aec09b8a4ba81166b3e933f69fd8a5b
JMustang <eecfredes@gmail.com>
2021-04-11 20:44:03 -0300

    curso de vuejs

---
+++ 01-DataBinding/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <h1 :style="{ textDecoration: decoration }">Hello {{name}}</h1>
+    <input type="text" v-model="name">
+    <br>
+    <a :href="link">Link para o meu github</a>
+    </div>
+</template>
+<script>
+
+export default {
+    data: () => ({
+        name:'junior',
+        decoration: 'underline',
+        link: 'https://github.com/JMustang'
+    })
+}
+
+
+
+
+</script>
+<style>
+
+</style>
---
+++ 02-diretivas/app.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+    <h1>Minha lista de Tarefas!</h1>
+    <button @click="() => showList = !showList">
+        ver lista
+    </button>
+    <br>
+    <input @ type="text" v-focus>
+    <ul v-if="showList">
+        <li
+            v-for="(task, index) in tasks"
+            :key="`${task}-${index}`"
+        >
+        {{task.name}}
+        </li>
+    </ul>
+    <p v-else>Lista escondida!</p>
+    </div>
+</template>
+<script>
+const focus = {
+    inserted: (el) => {
+        el.focus()
+    }
+}
+export default {
+    directives: {
+        focus
+    },
+    data: () => ({
+        showList: false,
+        tasks: [
+            {
+                name: 'Fazer curso',
+                isDone: false            
+            }
+        ]
+    })
+}
+
+</script>
+<style>
+
+</style>
